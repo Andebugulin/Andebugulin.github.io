8c5e981dd8c76cfe2c9e440e35490d388783643d
Andebugulin <gulin.andrey2005@gmail.com>
2026-01-29 10:03:27 +0200

    Styling of projects improved

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,12 +1,13 @@
-$(document).ready(function () {
-  // Init Masonry
-  var $grid = $(".grid").masonry({
-    gutter: 10,
-    horizontalOrder: true,
-    itemSelector: ".grid-item",
-  });
-  // Layout Masonry after each image loads
-  $grid.imagesLoaded().progress(function () {
-    $grid.masonry("layout");
-  });
-});
+// $(document).ready(function () {
+//   // Wait for images to load first
+//   var $grid = $(".grid").imagesLoaded(function() {
+//     // Then init Masonry
+//     $grid.masonry({
+//       gutter: 10,
+//       horizontalOrder: true,
+//       itemSelector: ".grid-item",
+//       columnWidth: 250,
+//       fitWidth: false
+//     });
+//   });
+// });
